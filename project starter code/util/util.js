cec5c2b5df0e1e9d0980efc157d7316fa437bc80
Alex <alex@qoco.co>
2024-03-17 17:53:19 +0100

    fix MIME problem for kitten image https://knowledge.udacity.com/questions/1026717

--- project starter code/util/util.js
+++ project starter code/util/util.js
@@ -1,3 +1,4 @@
+import axios from "axios"
 import fs from "fs";
 import Jimp from "jimp";
 
@@ -12,7 +13,10 @@ import Jimp from "jimp";
  export async function filterImageFromURL(inputURL) {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const photoBuffer = await axios.get(inputURL, {
+        responseType: "arraybuffer"
+      });
+      const photo = await Jimp.read(Buffer.from(photoBuffer?.data, "binary"));
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
